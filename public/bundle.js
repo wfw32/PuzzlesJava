532437136b4a682522c67b078856ee8fffe72a3a
wfw32 <113204970+wfw32@users.noreply.github.com>
2020-11-12 16:36:29 +0000

    Create 9.html

--- public/bundle.js
+++ public/bundle.js
@@ -1936,4 +1936,187 @@ var RecipeList = /*#__PURE__*/function (_Component) {
       if (!allIngredients) {
         return 'ingredients are no longer here...';
       } else {
-        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["ListGroup
+        return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["ListGroup"], {
+          className: "list-group-missing-ingredients"
+        }, allIngredients.map(function (item) {
+          return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["ListGroup"].Item, {
+            key: Math.random(),
+            as: "li",
+            className: missingIngredients.includes(item.toLowerCase().replace(/,/gi, '')) && _this.props.viewMissing ? 'test-class-yes missing-ingredients-text' : 'missing-ingredients-text'
+          }, item);
+        }));
+      }
+    }
+  }]);
+
+  return RecipeList;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+/* harmony default export */ __webpack_exports__["default"] = (RecipeList);
+
+/***/ }),
+
+/***/ "./client/components/results.js":
+/*!**************************************!*\
+  !*** ./client/components/results.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_bootstrap__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-bootstrap */ "./node_modules/react-bootstrap/esm/index.js");
+/* harmony import */ var react_apollo__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-apollo */ "./node_modules/react-apollo/react-apollo.esm.js");
+/* harmony import */ var apollo_boost__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! apollo-boost */ "./node_modules/apollo-boost/lib/bundle.esm.js");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  query($food: String) {\n    searchRecipes(food: $food) {\n      uri\n      url\n      label\n      image\n      calories\n      totalTime\n      ingredientLines\n    }\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+/* eslint-disable max-statements */
+
+/* eslint-disable complexity */
+
+
+
+
+
+var getMealsQuery = Object(apollo_boost__WEBPACK_IMPORTED_MODULE_3__["gql"])(_templateObject());
+
+var Results = /*#__PURE__*/function (_React$Component) {
+  _inherits(Results, _React$Component);
+
+  var _super = _createSuper(Results);
+
+  function Results() {
+    _classCallCheck(this, Results);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(Results, [{
+    key: "sendStringToQuery",
+    value: function sendStringToQuery() {
+      var timeString = '&time=1%2B';
+
+      if (this.props.location.state.time === '< 30 mins') {
+        timeString = '&time=1-30';
+      } else if (this.props.location.state.time === '< 60 mins') {
+        timeString = '&time=1-60';
+      } else if (this.props.location.state.time === '< 90 mins') {
+        timeString = '&time=1-90';
+      } else {
+        timeString = '&time=1-240';
+      }
+
+      var stringQuery = this.props.location.state.theMeats.concat(this.props.location.state.theSeafood).join('+').replace(/\s/g, '') + timeString;
+
+      if (this.props.location.state.theMeats.length === 0 && this.props.location.state.theMeats.length === 0) {
+        stringQuery = 'vegetarian' + timeString;
+      }
+
+      return stringQuery;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var theIngredients = this.props.location.state.theIngredients;
+      var food = this.sendStringToQuery();
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_apollo__WEBPACK_IMPORTED_MODULE_2__["Query"], {
+        query: getMealsQuery,
+        variables: {
+          food: food
+        }
+      }, function (_ref) {
+        var loading = _ref.loading,
+            error = _ref.error,
+            data = _ref.data;
+        if (loading) return 'Loading...';
+        if (error) return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Apologies, we've encountered an error! Please go back and try again!"));
+        var tracker = [];
+        var ourIngredientsArr = theIngredients;
+        var totalRecipesArr = data.searchRecipes;
+
+        var _loop = function _loop(i) {
+          var recipeIngredientsStr = totalRecipesArr[i].ingredientLines.join('').toLowerCase();
+          var lowerCasedIngredientsArr = ourIngredientsArr.map(function (x) {
+            return x.toLowerCase();
+          });
+          var filteredIngredients = lowerCasedIngredientsArr.filter(function (ingredient) {
+            return recipeIngredientsStr.includes(ingredient);
+          });
+          var percentage = filteredIngredients.length / totalRecipesArr[i].ingredientLines.length;
+          var filteredObj = {};
+          filteredObj.name = totalRecipesArr[i].label;
+          filteredObj.percent = Number(percentage);
+          tracker.push(filteredObj);
+          tracker.sort(function (a, b) {
+            return b.percent - a.percent;
+          });
+        };
+
+        for (var i = 0; i < totalRecipesArr.length; i++) {
+          _loop(i);
+        }
+
+        var renderArr = [];
+        var top5 = tracker.slice(0, 5);
+        var addedName = [];
+
+        if (totalRecipesArr.length !== 0) {
+          for (var _i = 0; _i < 5; _i++) {
+            var name = top5[_i].name;
+
+            for (var j = 0; j < totalRecipesArr.length; j++) {
+              if (totalRecipesArr[j].label === name) {
+                (function () {
+                  var obj = {};
+                  var lowerCasedIngredientsArr = ourIngredientsArr.map(function (x) {
+                    return x.toLowerCase();
+                  });
+
+                  var replaceCommas = function replaceCommas(str) {
+                    return str.replace(/,/gi, '');
+                  };
+
+                  var lowerCasedTotalRecipesArr = totalRecipesArr[j].ingredientLines.map(function (x) {
+                    return x.toLowerCase();
+                  });
+                  lowerCasedTotalRecipesArr = lowerCasedTotalRecipesArr.map(function (x) {
+                    return replaceCommas(x);
+                  });
+                  obj.calories = totalRecipesArr[j].calories;
+                  obj.image = totalRecipesArr[j].image;
+                  obj.url = totalReci
