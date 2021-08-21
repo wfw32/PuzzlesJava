af75b208891aea54f56280bf6a7998469a6137ad
wfw32 <113204970+wfw32@users.noreply.github.com>
2021-08-21 19:55:55 +0000

    removing files

--- public/bundle.js
+++ public/bundle.js
@@ -66817,4 +66817,335 @@ function getHostSibling(fiber) {
         continue siblings;
       } else {
         node.child.return = node;
-   
+        node = node.child;
+      }
+    } // Check if this host node is stable or about to be placed.
+
+
+    if (!(node.effectTag & Placement)) {
+      // Found it!
+      return node.stateNode;
+    }
+  }
+}
+
+function commitPlacement(finishedWork) {
+
+
+  var parentFiber = getHostParentFiber(finishedWork); // Note: these two variables *must* always be updated together.
+
+  var parent;
+  var isContainer;
+  var parentStateNode = parentFiber.stateNode;
+
+  switch (parentFiber.tag) {
+    case HostComponent:
+      parent = parentStateNode;
+      isContainer = false;
+      break;
+
+    case HostRoot:
+      parent = parentStateNode.containerInfo;
+      isContainer = true;
+      break;
+
+    case HostPortal:
+      parent = parentStateNode.containerInfo;
+      isContainer = true;
+      break;
+
+    case FundamentalComponent:
+
+    // eslint-disable-next-line-no-fallthrough
+
+    default:
+      {
+        {
+          throw Error( "Invalid host parent fiber. This error is likely caused by a bug in React. Please file an issue." );
+        }
+      }
+
+  }
+
+  if (parentFiber.effectTag & ContentReset) {
+    // Reset the text content of the parent before doing any insertions
+    resetTextContent(parent); // Clear ContentReset from the effect tag
+
+    parentFiber.effectTag &= ~ContentReset;
+  }
+
+  var before = getHostSibling(finishedWork); // We only have the top Fiber that was inserted but we need to recurse down its
+  // children to find all the terminal nodes.
+
+  if (isContainer) {
+    insertOrAppendPlacementNodeIntoContainer(finishedWork, before, parent);
+  } else {
+    insertOrAppendPlacementNode(finishedWork, before, parent);
+  }
+}
+
+function insertOrAppendPlacementNodeIntoContainer(node, before, parent) {
+  var tag = node.tag;
+  var isHost = tag === HostComponent || tag === HostText;
+
+  if (isHost || enableFundamentalAPI ) {
+    var stateNode = isHost ? node.stateNode : node.stateNode.instance;
+
+    if (before) {
+      insertInContainerBefore(parent, stateNode, before);
+    } else {
+      appendChildToContainer(parent, stateNode);
+    }
+  } else if (tag === HostPortal) ; else {
+    var child = node.child;
+
+    if (child !== null) {
+      insertOrAppendPlacementNodeIntoContainer(child, before, parent);
+      var sibling = child.sibling;
+
+      while (sibling !== null) {
+        insertOrAppendPlacementNodeIntoContainer(sibling, before, parent);
+        sibling = sibling.sibling;
+      }
+    }
+  }
+}
+
+function insertOrAppendPlacementNode(node, before, parent) {
+  var tag = node.tag;
+  var isHost = tag === HostComponent || tag === HostText;
+
+  if (isHost || enableFundamentalAPI ) {
+    var stateNode = isHost ? node.stateNode : node.stateNode.instance;
+
+    if (before) {
+      insertBefore(parent, stateNode, before);
+    } else {
+      appendChild(parent, stateNode);
+    }
+  } else if (tag === HostPortal) ; else {
+    var child = node.child;
+
+    if (child !== null) {
+      insertOrAppendPlacementNode(child, before, parent);
+      var sibling = child.sibling;
+
+      while (sibling !== null) {
+        insertOrAppendPlacementNode(sibling, before, parent);
+        sibling = sibling.sibling;
+      }
+    }
+  }
+}
+
+function unmountHostComponents(finishedRoot, current, renderPriorityLevel) {
+  // We only have the top Fiber that was deleted but we need to recurse down its
+  // children to find all the terminal nodes.
+  var node = current; // Each iteration, currentParent is populated with node's host parent if not
+  // currentParentIsValid.
+
+  var currentParentIsValid = false; // Note: these two variables *must* always be updated together.
+
+  var currentParent;
+  var currentParentIsContainer;
+
+  while (true) {
+    if (!currentParentIsValid) {
+      var parent = node.return;
+
+      findParent: while (true) {
+        if (!(parent !== null)) {
+          {
+            throw Error( "Expected to find a host parent. This error is likely caused by a bug in React. Please file an issue." );
+          }
+        }
+
+        var parentStateNode = parent.stateNode;
+
+        switch (parent.tag) {
+          case HostComponent:
+            currentParent = parentStateNode;
+            currentParentIsContainer = false;
+            break findParent;
+
+          case HostRoot:
+            currentParent = parentStateNode.containerInfo;
+            currentParentIsContainer = true;
+            break findParent;
+
+          case HostPortal:
+            currentParent = parentStateNode.containerInfo;
+            currentParentIsContainer = true;
+            break findParent;
+
+        }
+
+        parent = parent.return;
+      }
+
+      currentParentIsValid = true;
+    }
+
+    if (node.tag === HostComponent || node.tag === HostText) {
+      commitNestedUnmounts(finishedRoot, node, renderPriorityLevel); // After all the children have unmounted, it is now safe to remove the
+      // node from the tree.
+
+      if (currentParentIsContainer) {
+        removeChildFromContainer(currentParent, node.stateNode);
+      } else {
+        removeChild(currentParent, node.stateNode);
+      } // Don't visit children because we already visited them.
+
+    } else if (node.tag === HostPortal) {
+      if (node.child !== null) {
+        // When we go into a portal, it becomes the parent to remove from.
+        // We will reassign it back when we pop the portal on the way up.
+        currentParent = node.stateNode.containerInfo;
+        currentParentIsContainer = true; // Visit children because portals might contain host components.
+
+        node.child.return = node;
+        node = node.child;
+        continue;
+      }
+    } else {
+      commitUnmount(finishedRoot, node, renderPriorityLevel); // Visit children because we may find more host components below.
+
+      if (node.child !== null) {
+        node.child.return = node;
+        node = node.child;
+        continue;
+      }
+    }
+
+    if (node === current) {
+      return;
+    }
+
+    while (node.sibling === null) {
+      if (node.return === null || node.return === current) {
+        return;
+      }
+
+      node = node.return;
+
+      if (node.tag === HostPortal) {
+        // When we go out of the portal, we need to restore the parent.
+        // Since we don't keep a stack of them, we will search for it.
+        currentParentIsValid = false;
+      }
+    }
+
+    node.sibling.return = node.return;
+    node = node.sibling;
+  }
+}
+
+function commitDeletion(finishedRoot, current, renderPriorityLevel) {
+  {
+    // Recursively delete all host nodes from the parent.
+    // Detach refs and call componentWillUnmount() on the whole subtree.
+    unmountHostComponents(finishedRoot, current, renderPriorityLevel);
+  }
+
+  detachFiber(current);
+}
+
+function commitWork(current, finishedWork) {
+
+  switch (finishedWork.tag) {
+    case FunctionComponent:
+    case ForwardRef:
+    case MemoComponent:
+    case SimpleMemoComponent:
+    case Block:
+      {
+        // Layout effects are destroyed during the mutation phase so that all
+        // destroy functions for all fibers are called before any create functions.
+        // This prevents sibling component effects from interfering with each other,
+        // e.g. a destroy function in one component should never override a ref set
+        // by a create function in another component during the same commit.
+        commitHookEffectListUnmount(Layout | HasEffect, finishedWork);
+        return;
+      }
+
+    case ClassComponent:
+      {
+        return;
+      }
+
+    case HostComponent:
+      {
+        var instance = finishedWork.stateNode;
+
+        if (instance != null) {
+          // Commit the work prepared earlier.
+          var newProps = finishedWork.memoizedProps; // For hydration we reuse the update path but we treat the oldProps
+          // as the newProps. The updatePayload will contain the real change in
+          // this case.
+
+          var oldProps = current !== null ? current.memoizedProps : newProps;
+          var type = finishedWork.type; // TODO: Type the updateQueue to be specific to host components.
+
+          var updatePayload = finishedWork.updateQueue;
+          finishedWork.updateQueue = null;
+
+          if (updatePayload !== null) {
+            commitUpdate(instance, updatePayload, type, oldProps, newProps);
+          }
+        }
+
+        return;
+      }
+
+    case HostText:
+      {
+        if (!(finishedWork.stateNode !== null)) {
+          {
+            throw Error( "This should have a text node initialized. This error is likely caused by a bug in React. Please file an issue." );
+          }
+        }
+
+        var textInstance = finishedWork.stateNode;
+        var newText = finishedWork.memoizedProps; // For hydration we reuse the update path but we treat the oldProps
+        // as the newProps. The updatePayload will contain the real change in
+        // this case.
+
+        var oldText = current !== null ? current.memoizedProps : newText;
+        commitTextUpdate(textInstance, oldText, newText);
+        return;
+      }
+
+    case HostRoot:
+      {
+        {
+          var _root = finishedWork.stateNode;
+
+          if (_root.hydrate) {
+            // We've just hydrated. No need to hydrate again.
+            _root.hydrate = false;
+            commitHydratedContainer(_root.containerInfo);
+          }
+        }
+
+        return;
+      }
+
+    case Profiler:
+      {
+        return;
+      }
+
+    case SuspenseComponent:
+      {
+        commitSuspenseComponent(finishedWork);
+        attachSuspenseRetryListeners(finishedWork);
+        return;
+      }
+
+    case SuspenseListComponent:
+      {
+        attachSuspenseRetryListeners(finishedWork);
+        return;
+      }
+
+    case Inco
